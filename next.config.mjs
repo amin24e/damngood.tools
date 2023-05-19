2615584db0aad1324593c69884da4e4bae695fcd
Amin24e <amin24e@gmail.com>
2023-05-19 21:42:27 +0100

    Update next.config.mjs
    The issue is that the next.config.mjs is using a mix of ESM (ECMAScript modules) and CJS (CommonJS) module syntax. require and module.exports are not valid in ESM, but import and export are.
    
    Here is how you can convert your next.config.mjs to use ESM:

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,6 @@
+// next.config.mjs
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
@@ -5,10 +8,11 @@ const nextConfig = {
         appDir: true,
     },
     webpack: (config) => {
+        config.resolve.alias['@'] = path.resolve(process.cwd(), './src');
         config.externals = [...config.externals, "canvas", "hnswlib-node"]
 
         return config
     },
 }
 
-export default nextConfig
+export default nextConfig;
